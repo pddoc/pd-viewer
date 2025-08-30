41e32f7a6feb32ddb8f54ea324b6eb4e468dbaf4
gengkaipeng <1002934864@qq.com>
2025-08-30 17:19:23 +0800

    添加react示例

---
+++ react-demo/src/App.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react'
+import PdfViewer from "pd-viewer";
+import './index.css'
+
+function App() {
+  let viewer;
+  useEffect(() => {
+    if (!viewer) {
+      viewer = new PdfViewer("viewer-id");
+      // 以文档尺寸80%的比例渲染
+      // const viewer = new PdfViewer("viewer-id", 0.8);
+
+      // 显示网络文件
+      viewer.render("./a.pdf");
+    }
+  }, [])
+
+  // 显示本地文件
+  const doChooseFile = (e) => {
+    const file = e.target.files[0];
+    viewer.render(file);
+  };
+  // 放大
+  const doZoomIn = () => {
+    viewer.zoomIn();
+  };
+  // 缩小
+  const doZoomOut = () => {
+    viewer.zoomOut();
+  };
+
+  // 上一页
+  const doPrePage = () => {
+    viewer.goPrePage();
+  };
+  // 下一页
+  const doNextPage = () => {
+    viewer.goNextPage();
+  };
+
+  return (
+    <>
+      <h1>PdViewer - 简单易用的pdf阅读器(基于pdfjs-dist封装)</h1>
+      <h4>
+        <a href="https://www.pddoc.cn" alt="pdf在线编辑器" target="_blank"
+        >pdf在线编辑器：www.pddoc.cn</a
+        >
+      </h4>
+
+      <input type="file" onChange={doChooseFile} />
+      <input type="button" value="放 大" onClick={doZoomIn} />&nbsp;
+      <input type="button" value="缩 小" onClick={doZoomOut} />&nbsp;
+      <input type="button" value="上一页" onClick={doPrePage} />&nbsp;
+      <input type="button" value="下一页" onClick={doNextPage} />
+      <div id="viewer-id"></div>
+    </>
+  )
+}
+
+export default App
